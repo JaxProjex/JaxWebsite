473a17e43f0882f207a8b3443208f35ebdd603d6
JaxProjex <strattonja@protonmail.com>
2024-09-19 22:50:39 -0700

    adjusting styles, projex drop

--- src/Components/Projex.tsx
+++ src/Components/Projex.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {useNavigate} from "react-router-dom";
 import '../CSS/Projex.css';
 import Header from "../Components/Header";
@@ -7,19 +7,38 @@ import Footer from "../Components/Footer";
 import ProjexDrop from "../Components/ProjexDrop";
 
 function Projex() {
+
+    const [revealOpen, setRevealOpen] = useState(0);
+
+    function handleClick(num:number) {
+        window.alert(num)
+        setRevealOpen(num);
+    }
+
     return (
         <div className={"projex-wrapper"}>
             <Header title={"projex"}/>
             <TopNav title={"projex"}/>
             <div className={"projex"}>
+                if (revealOpen === 0) { 
                 <div className={"projex-grid-container"}>
-                <ProjexDrop project={"Manifest Application"}/>
-                    <ProjexDrop project={"Skyview-RPi"}/>
-                    <ProjexDrop project={"ATAK DACO Plugin"}/>
-                    <ProjexDrop project={"75th Ranger Regiment Website"}/>
-                    <ProjexDrop project={"Node-Red TAK integrations"}/>
-                    <ProjexDrop project={"Kismet ATAK Companion"}/>
+                    <ProjexDrop project={"Manifest Application"} onClick={() => handleClick(1)}/>
+                    <ProjexDrop project={"Skyview-RPi"} onClick={() => handleClick(2)}/>
+                    <ProjexDrop project={"ATAK DACO Plugin"} onClick={() => handleClick(3)}/>
+                    <ProjexDrop project={"75th Ranger Regiment Website"} onClick={() => handleClick(4)}/>
+                    <ProjexDrop project={"Node-Red TAK integrations"} onClick={() => handleClick(5)}/>
+                    <ProjexDrop project={"Kismet ATAK Companion"} onClick={() => handleClick(6)}/>
                 </div>
+                } else {
+                <div className={"projex-display-container"}>
+                {revealOpen === 1 && <>hello</>}
+                {revealOpen === 2 && <>greetigs</>}
+                {revealOpen === 3 && <>hi</>}
+                {revealOpen === 4 && <>hola</>}
+                {revealOpen === 5 && <>aloha</>}
+                {revealOpen === 6 && <>hey</>}
+                </div>
+                }
             </div>
             <Footer/>
         </div>
